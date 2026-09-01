6b733ab607525c6aaba6d673c84fee1c55584a97
Test Runner <test@ppp.local>
2026-09-01 19:52:24 +0000

    test(relay): cover pool migration rollback

--- server/migrations/297_relay_stage_agent_pool_integrity.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TRIGGER IF EXISTS relay_stage_agent_pool_validate_member_trigger
-    ON relay_stage_agent_pool;
-DROP FUNCTION IF EXISTS relay_stage_agent_pool_validate_member();
-ALTER TABLE relay_stage_agent_pool DROP COLUMN IF EXISTS last_selected_at;
